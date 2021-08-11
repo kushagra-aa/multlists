00baa7fe30cc2c2e63ceabd68cc8156bc4d9b8b7
kushagra-aa <kushagraagnihotri24@gmail.com>
2021-08-12 01:59:52 +0530

    Heroku Added

---
+++ Procfile
@@ -0,0 +1,1 @@
+web: gunicorn mult-lists.wsgi
--- multlists/settings.py
+++ multlists/settings.py
@@ -12,6 +12,7 @@ https://docs.djangoproject.com/en/3.2/ref/settings/
 
 from pathlib import Path
 import os
+import django_heroku
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -125,3 +126,5 @@ STATICFILES_DIRS = (os.path.join(BASE_DIR, 'static/'),)
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+django_heroku.settings(locals())
